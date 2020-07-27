047c7ace1e8f87c62aae7830c78803912d0a753d
Ubuntu <ubuntu@ip-172-31-3-188.ap-southeast-1.compute.internal>
2020-07-27 07:46:46 +0000

    Edits to mssql queries

--- server.js
+++ server.js
@@ -3,34 +3,14 @@ var fs = require('fs');
 var sql = require("mssql");
 var config = {
   user: 'sa',
-  password: '1qaz2wsxXS@',
-  server: 'LAPTOP-C5J86CBA\\SQLEXPRESS',
-  database: 'forsa',
+  password: 'Q1w2e3r4',
+  server: 'localhost',
+  database: 'sophia',
   options: {
     enableArithAbort: true,
     encrypt: true
   },
 };
-var conn = new sql.ConnectionPool(config);
-var req = new sql.Request(conn);
-
-conn.connect(function (err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  req.query("SELECT * From satable", function (err, recordset) {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      console.log(recordset["recordset"]);
-      var temp= recordset["recordset"];
-    }
-    conn.close();
-  });
-});
-
 http.createServer(function (req, res) {
     var url = req.url;
     if (url === '/api') {
@@ -40,9 +20,20 @@ http.createServer(function (req, res) {
         // Prepare your data here and return JSON
         // results below is an example of data to render into UI
 
-        var results = temp;
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(results));
+
+	sql.connect(config, function(err) {
+		if (err) {
+			console.log(err);
+		}
+		var req = new sql.Request();
+		req.query('SELECT * FROM Response', function (err, recordset) {
+			if (err) {
+				console.log(err);
+			}
+			res.writeHead(200, {'Content-Type': 'application/json'});
+			res.end(JSON.stringify(recordset));
+		});
+	});
 
     } else if (url === '/') {
         // Homepage for yuor UI. URL is root '/'
